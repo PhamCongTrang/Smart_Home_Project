bbc60ab0f755342e20348ae8a51903ed237a1d39
PhamTrang <trang.pc200631@sis.hust.edu.vn>
2024-01-12 18:19:29 +0000

    Trang add all

--- test_esp/src/main.cpp
+++ test_esp/src/main.cpp
@@ -1,18 +1,29 @@
+// Include necessary libraries
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define an array of GPIO pins to test
+const int ledPins[] = {D0, D1, D2, D3, D4, D5, D6, D7, D8};
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  // Set all pins as outputs
+  Serial.begin(9600);
+  for (int pin : ledPins)
+  {
+    pinMode(pin, OUTPUT);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  // Iterate through each pin and toggle the LED
+  for (int pin : ledPins)
+  {
+    digitalWrite(pin, HIGH);
+    Serial.print("Pin:"); Serial.println(pin);
+    delay(500); // Wait for 0.5 seconds (adjust as needed)
+    digitalWrite(pin, LOW);
+    delay(500); // Wait for 0.5 seconds (adjust as needed)
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  }
 }
